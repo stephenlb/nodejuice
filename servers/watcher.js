c1b7b440386026c3c20576bbba650a1255a94682
Stephen L. Blum <blum.stephen@gmail.com>
2009-12-24 10:55:10 -0800

    applied final touches to watcher server and it looks good. started to play with browser client side with xdr3 lib, more work needed.

--- servers/watcher.js
+++ servers/watcher.js
@@ -8,16 +8,34 @@ var sys         = require('sys')
 // Start By Loading Config and Directories
 posix.cat('./configure/watcher.json.js').addCallback(function(config) {
     config = JSON.parse(config);
-    // sys.print(sys.inspect(config));
+
+    http.createServer(function (req, res) {
+        callbacks.push(function(wait) {
+            res.sendHeader( 200, {"Content-Type": "text/plain"} );
+            res.sendBody(wait);
+            res.finish();
+        });
+
+        sys.puts('\nCallback Length: ' + callbacks.length);
+        sys.puts('\nConnections: '     + connections++);
+    }).listen(config.port);
 
     recdirectory( config.watch, config.ignore, function( file, stats ) {
         sys.puts(file);
+
+        process.watchFile( file, function(curr, prev) {
+            // if ( curr.size == prev.size ) return;
+
+            sys.puts('\nfile: ' + file + ' length: ' + callbacks.length);
+            while (callbacks.length > 0) {
+                callbacks.shift()(content + ' length: ' + callbacks.length);
+            }
+        } );
     } );
 });
 
-// Recursive Directory File Gathering
+// Non Blocking Recursive Directory
 var recdirectory = function( start, ignore, callback ) {
-    // sys.puts( ' 1 <-----> ' + start);
     posix.readdir(start).addCallback(function(files) {
         files.forEach(function(file) {
             // Ignored Files/Directories
@@ -25,93 +43,8 @@ var recdirectory = function( start, ignore, callback ) {
                 return (new RegExp(item)).test(file)
             }).length) return;
 
-            recdirectory(
-                start + '/' + file, ignore, callback
-            );
-
+            recdirectory( start + '/' + file, ignore, callback );
             callback(start + '/' + file);
         });
     });
-    // sys.puts( ' 2 <-----> ' + start);
-    // Get Status of Each File
-    // sys.puts(sys.inspect(files));
-    /*
-    files.forEach(function(file) {
-        // sys.puts(file);
-        // Ignore This File/Dir?
-        if (ignore.filter(function(item) {
-            return (new RegExp(item)).test(file)
-        }).length) return;
-        // sys.puts(file);
-        var stats = posix.stat(file).wait();
-        if ( stats.isDirectory() ){
-// sys.puts('------>' + file);
-// sys.puts(start + '/' + file);
-        return recdirectory(
-            start + '/' + file, ignore, callback
-        );
-        }
-        callback(file);
-    });
-    */
 };
-
-// Start Watcher Server
-/*
-http.createServer(function (req, res) {
-    callbacks.push(function(wait) {
-        res.sendHeader( 200, {"Content-Type": "text/plain"} );
-        res.sendBody(wait);
-        res.finish();
-    });
-
-    sys.puts('\nCallback Length: ' + callbacks.length);
-    sys.puts('\nConnections: '     + connections++);
-}).listen(8002);
-*/
-
-
-
-/*
-
-posix.readdir('/opt/nodejuice/').addCallback( function(dirs) {
-    sys.print(sys.inspect(dirs));
-} );
-*/
-
-/*
-http.createServer(function (req, res) {
-    res.sendHeader(200, {"Content-Type": "text/plain"});
-    res.sendBody("Hello World\n");
-    res.finish();
-}).listen(8000);
-*/
-
-/*
-    plan:
-    ------
-    load json files
-
-    recursive dir to get all files and watch them.
-        attache them all to same event listenter
-
-+posix.readdir(path)+ ::
-+posix.cat(filename, encoding="utf8")+::
-*/
-
-/*
-
-*/
-
-/*
-process.watchFile( file, function(curr, prev) {
-    if ( curr.size == prev.size ) return;
-
-    while (callbacks.length > 0) {
-        sys.puts('\nlength: ' + callbacks.length);
-        callbacks.shift()(content + ' length: ' + callbacks.length);
-    }
-} );
-sys.puts("Server running at http://127.0.0.1:8000/");
-
-*/
