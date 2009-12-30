29e6e97fe808b701e0ac45218c5ff1f709841f4d
Stephen L. Blum <blum.stephen@gmail.com>
2009-12-29 23:53:49 -0800

    added website with full prettiness and explanation of nodeJuice. performed cleanup on few files.

---
+++ website/configure/seeker.js
@@ -0,0 +1,10 @@
+exports.seeker = {
+    host    : null,
+    port    : 8002,
+    ignore  : [
+        /git$/,
+        /svn$/,
+        /cvs$/,
+        /swp$/
+    ]
+};
---
+++ website/configure/wsgi.js
@@ -0,0 +1,6 @@
+exports.wsgi = {
+    host : null,
+    port : 80,
+    root : 'index.htm',
+    url  : [ [/^\/.*?\/?/, '/'] ]
+};
